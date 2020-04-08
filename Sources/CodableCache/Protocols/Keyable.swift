7fb6aebab20ff44adc4b9786feb2ed18247c5738
Jeremy Greenwood <jeremy@mobelux.com>
2020-04-08 16:49:17 -0400

    ooops fixed md5

--- Sources/CodableCache/Protocols/Keyable.swift
+++ Sources/CodableCache/Protocols/Keyable.swift
@@ -13,12 +13,12 @@ public protocol Keyable {
 
 extension URL: Keyable {
     public var key: String {
-        return String(hashValue).md5
+        return absoluteString.md5
     }
 }
 
 extension String: Keyable {
     public var key: String {
-        return String(hashValue).md5
+        return md5
     }
 }
