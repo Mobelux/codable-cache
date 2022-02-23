1b841e62741e5a40ff35501552515d5c0a68851f
Jeremy Greenwood <jeremy@mobelux.com>
2022-02-23 16:35:43 -0500

    add docs

--- Sources/CodableCache/CodableCache.swift
+++ Sources/CodableCache/CodableCache.swift
@@ -25,23 +25,34 @@ public final class CodableCache {
 
     private let cache: Cache
 
+    /// Initilize and instance of `CodableCache`
+    /// - Parameter cache: A type conforming to the `Cache` protocol.
     public init(_ cache: Cache) {
         self.cache = cache
     }
 
+    /// Asynchronously caches the given object
+    /// - Parameter object: The object which should be cached. It must conform to `Codable`.
+    /// - Parameter key: A unique key used to identify the cached object.
+    /// - Parameter ttl: Defines the amount of time the cached object is valid.
     public func cache<T: Codable>(object: T, key: Keyable, ttl: TTL = TTL.default) async throws {
         let wrapper = CacheWrapper(ttl: ttl, created: Date(), object: object)
         try await cache.cache(try encoder.encode(wrapper), key: key.rawValue)
     }
 
+    /// Deletes the cached object associated with the given key
+    /// - Parameter key: A unique key used to identify the cached object
     public func delete(objectWith key: Keyable) async throws {
         try await cache.delete(key.rawValue)
     }
 
+    /// Deletes all cached objects
     public func deleteAll() async throws {
         try await cache.deleteAll()
     }
 
+    /// Asynchronously gets an object from the cache.
+    /// - Returns: An instance of the previously cached object. If the `ttl` has expired or if nothing has been cached for `key`, returns nil
     public func object<T: Codable>(key: Keyable) async -> T? {
         do {
             let data = try await self.cache.data(key.rawValue)
--- Sources/CodableCache/CodableCaching.swift
+++ Sources/CodableCache/CodableCaching.swift
@@ -21,16 +21,18 @@ public final class CodableCaching<Value: Codable> {
     private let cache: () throws -> Cache
     private let key: Keyable
     private let ttl: TTL
-    private var value: Value?
 
+    public var wrappedValue: Value?
     public var projectedValue: CodableCaching<Value> { self }
 
+    /// Asynchronously gets the value from the cache. If the `ttl` has expired or if nothing has been cached for `key`, returns nil
     public func get() async -> Value? {
         let cachedValue: Value? = await codableCache.object(key: key)
-        value = cachedValue
+        wrappedValue = cachedValue
         return cachedValue
     }
 
+    /// Asynchronously caches the given value
     public func set(_ value: Value?) async {
         do {
             if value == nil {
@@ -39,7 +41,7 @@ public final class CodableCaching<Value: Codable> {
                 try await codableCache.cache(object: value, key: key, ttl: ttl)
             }
 
-            self.value = value
+            self.wrappedValue = value
         } catch let error as NSError {
             switch error.code {
             case NSFileNoSuchFileError, NSFileReadNoSuchFileError: break
@@ -51,19 +53,19 @@ public final class CodableCaching<Value: Codable> {
         }
     }
 
-    public var wrappedValue: Value? {
-        get { value }
-        set { value = newValue }
-    }
-
+    /// Initializes an instance of `CodableCaching`
+    /// - Parameters:
+    ///   - wrappedValue: A default value
+    ///   - key: A unique key used to identify the cached object.
+    ///   - cache: A function defining a type conforming to `Cache` to use as backing storage.
+    ///   - ttl: Defines the amount of time the cached object is valid.
     public init(wrappedValue: Value? = nil,
                 key: Keyable,
                 cache: @escaping () throws -> Cache = { try DiskCache(storageType: .temporary(.custom("codable-cache"))) },
                 ttl: TTL = .default) {
-        self.value = wrappedValue
+        self.wrappedValue = wrappedValue
         self.key = key
         self.cache = cache
         self.ttl = ttl
     }
 }
-
