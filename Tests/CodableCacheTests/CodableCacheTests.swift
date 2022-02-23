e7d8436b78e2af85fb3a49c9c2d50ccfd43ccb68
Jeremy Greenwood <jeremy@mobelux.com>
2022-02-23 16:37:53 -0500

    remove linux test files

--- Tests/CodableCacheTests/CodableCacheTests.swift
+++ Tests/CodableCacheTests/CodableCacheTests.swift
@@ -3,8 +3,4 @@ import XCTest
 
 final class CodableCacheTests: XCTestCase {
     func testExample() { }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import codable_cacheTests
-
-var tests = [XCTestCaseEntry]()
-tests += codable_cacheTests.allTests()
-XCTMain(tests)
