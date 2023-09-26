134bf8b0e7059ace21277f7833f4d70adbe22e1e
Jeremy Greenwood <jeremy@mobelux.com>
2023-09-26 15:25:32 -0400

    use https for dependencies

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "git@github.com:Mobelux/DiskCache.git", from: "1.0.0"),
-         .package(url: "git@github.com:apple/swift-crypto.git", from: "1.0.0")
+         .package(url: "https://github.com/Mobelux/DiskCache.git", from: "1.0.0"),
+         .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
