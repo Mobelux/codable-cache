0baace54097ce66855cac4ce7377568b48a814bf
Mathew Gacy <matt@mgacy.com>
2024-07-03 08:25:29 -0700

    Additional Documentation and DocC Support (#5)
    * Add punctuation
    
    * Add documentation for remaining public members
    
    * Add basic documentation catalog
    
    * Add SPI manifest

--- Sources/CodableCache/Models/TTL.swift
+++ Sources/CodableCache/Models/TTL.swift
@@ -1,23 +1,30 @@
 //
 //  TTL.swift
-//  
+//
 //
 //  Created by Jeremy Greenwood on 3/30/20.
 //
 
 import Foundation
 
+/// Specifies the time-to-live for a cached object.
 public enum TTL: Codable {
     private enum CodingKeys: String, CodingKey {
         case second, minute, hour, day, forever
     }
 
+    /// A `TTL` representing a given number of seconds.
     case second(Int)
+    /// A `TTL` representing a given number of minutes.
     case minute(Int)
+    /// A `TTL` representing a given number of hours.
     case hour(Int)
+    /// A `TTL` representing a given number of days.
     case day(Int)
+    /// A `TTL` representing a cached object that never expires.
     case forever
 
+    /// The default `TTL` value.
     public static let `default` = TTL.day(1)
 
     var value: Int {
--- Sources/CodableCache/Protocols/Keyable.swift
+++ Sources/CodableCache/Protocols/Keyable.swift
@@ -1,13 +1,15 @@
 //
 //  Keyable.swift
-//  
+//
 //
 //  Created by Jeremy Greenwood on 3/30/20.
 //
 
 import Foundation
 
+/// A class of types that can be used as a key in a cache.
 public protocol Keyable {
+    /// The raw value of the key.
     var rawValue: String { get }
 }
 
